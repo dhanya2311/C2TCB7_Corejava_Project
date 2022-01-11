17545f537812cac99b917c00d1e8959a022d34d5
dhanya2311 <70582137+dhanya2311@users.noreply.github.com>
2022-01-10 22:12:07 -0800

    added exception package

---
+++ src/Exception_Day_15/Testtrycatch1.java
@@ -0,0 +1,9 @@
+package Exception_Day_15;
+
+public class Testtrycatch1 {
+public static void main(String[] args)
+{
+	int data=50/0;  //may throw exception
+	System.out.println("rest of the code.....");
+}
+}
