ec30c770c418e6622fec666361bb5eb8a88b68d1
dhanya2311 <dhanyahm2397@gmail.com>
2022-01-24 20:58:46 +0530

    added generic and string buffer packages

---
+++ src/Generic_Day_19/Genericmethodtest1.java
@@ -0,0 +1,23 @@
+package Generic_Day_19;
+
+public class Genericmethodtest1 {
+
+	
+   public <E>void displayArrayElements(E[] elements)
+   {
+	  for(E element:elements) 
+	  {
+		  System.out.println("element is: "+element);
+	  }
+   }
+	public static void main(String[] args) 
+	{
+		GenericMethodTest obj=new GenericMethodTest();
+	Integer[] intarray= {10,20,30,40};
+	String[] str= {"java","programming", "learning"};
+	
+	obj.displayArrayElements(intarray);
+	obj.displayArrayElements(str);
+	}
+
+}
